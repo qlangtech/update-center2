8dc653ff2789ba528f96f97eb779b40f24b534f4
mozhenghua <baisui@qlangtech.com>
2022-01-04 13:53:01 +0800

    modfiy build script adding resources dir

--- src/main/java/io/jenkins/update_center/DefaultMavenRepositoryBuilder.java
+++ src/main/java/io/jenkins/update_center/DefaultMavenRepositoryBuilder.java
@@ -1,6 +1,7 @@
 package io.jenkins.update_center;
 
 import com.qlangetch.tis.impl.TISAliyunOSSRepositoryImpl;
+import com.qlangetch.tis.impl.TISLocalFileRepositoryImpl;
 
 public class DefaultMavenRepositoryBuilder {
 
@@ -16,7 +17,8 @@ public class DefaultMavenRepositoryBuilder {
     public static BaseMavenRepository getInstance() {
         if (instance == null) {
             // if (ARTIFACTORY_API_PASSWORD != null && ARTIFACTORY_API_USERNAME != null) {
-            instance = new TISAliyunOSSRepositoryImpl(); //new ArtifactoryRepositoryImpl(ARTIFACTORY_API_USERNAME, ARTIFACTORY_API_PASSWORD);
+           // instance = new TISAliyunOSSRepositoryImpl(); //new ArtifactoryRepositoryImpl(ARTIFACTORY_API_USERNAME, ARTIFACTORY_API_PASSWORD);
+            instance = new TISLocalFileRepositoryImpl();
 //            } else {
 //                throw new IllegalStateException("ARTIFACTORY_USERNAME and ARTIFACTORY_PASSWORD need to be set");
 //            }
