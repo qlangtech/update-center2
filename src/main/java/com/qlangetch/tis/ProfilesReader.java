f23969bb6e4968d8701062d7a1b2f6fbfc456440
mozhenghua <baisui@qlangtech.com>
2024-10-16 16:20:21 +0800

    set targetProfileProperties for immuatable

--- src/main/java/com/qlangetch/tis/ProfilesReader.java
+++ src/main/java/com/qlangetch/tis/ProfilesReader.java
@@ -1,5 +1,6 @@
 package com.qlangetch.tis;
 
+import com.google.common.collect.Maps;
 import com.qlangtech.tis.maven.plugins.tpi.PluginClassifier;
 import org.apache.commons.collections.MapUtils;
 import org.apache.maven.model.Model;
@@ -22,37 +23,39 @@ public class ProfilesReader {
 
     public static PluginClassifier readeProfile() {
 
-        File mvnHome = new File("/opt/data/mvn_repository");
-        File pomFilePath = new File(mvnHome, "com/qlangtech/tis/plugins/tis-plugin-parent/4.0.1/tis-plugin-parent-4.0.1.pom");
-        if (!pomFilePath.exists()) {
-            throw new IllegalStateException("pomFilePath can not be exist:" + pomFilePath);
-        }
-        final String targetProfile = "default-emr";
-        Map<String, String> targetProfileProperties = null;
-        try (FileReader reader = new FileReader(pomFilePath)) {
-            MavenXpp3Reader mavenReader = new MavenXpp3Reader();
-            Model model = mavenReader.read(reader);
-
-            List<Profile> profiles = model.getProfiles();
-            for (Profile profile : profiles) {
-                if (!targetProfile.equalsIgnoreCase(profile.getId())) {
-                    continue;
-                }
-                targetProfileProperties = new HashMap<>();
-                for (Map.Entry entry : profile.getProperties().entrySet()) {
-                    targetProfileProperties.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
-                }
-//                System.out.println("Profile ID: " + profile.getId());
-//                System.out.println("Activation: " + profile.getActivation());
-//                System.out.println("Properties: " + profile.getProperties());
-//                // 可以继续打印其他信息
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        if (MapUtils.isEmpty(targetProfileProperties)) {
-            throw new IllegalStateException("targetProfile:" + targetProfile + " pomfile:" + pomFilePath.getAbsolutePath() + " relevant ");
-        }
+//        File mvnHome = new File("/opt/data/mvn_repository");
+//        File pomFilePath = new File(mvnHome, "com/qlangtech/tis/plugins/tis-plugin-parent/4.0.1/tis-plugin-parent-4.0.1.pom");
+//        if (!pomFilePath.exists()) {
+//            throw new IllegalStateException("pomFilePath can not be exist:" + pomFilePath);
+//        }
+//        final String targetProfile = "default-emr";
+//        Map<String, String> targetProfileProperties = null;
+//        try (FileReader reader = new FileReader(pomFilePath)) {
+//            MavenXpp3Reader mavenReader = new MavenXpp3Reader();
+//            Model model = mavenReader.read(reader);
+//
+//            List<Profile> profiles = model.getProfiles();
+//            for (Profile profile : profiles) {
+//                if (!targetProfile.equalsIgnoreCase(profile.getId())) {
+//                    continue;
+//                }
+//                targetProfileProperties = new HashMap<>();
+//                for (Map.Entry entry : profile.getProperties().entrySet()) {
+//                    targetProfileProperties.put(String.valueOf(entry.getKey()), String.valueOf(entry.getValue()));
+//                }
+//
+//            }
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
+        Map<String, String> targetProfileProperties = Maps.newHashMap();
+//        if (MapUtils.isEmpty(targetProfileProperties)) {
+//            throw new IllegalStateException("targetProfile:" + targetProfile + " pomfile:" + pomFilePath.getAbsolutePath() + " relevant ");
+//        }
+        targetProfileProperties.put("hudi", "0.14.1");
+        targetProfileProperties.put("spark", "2.4.4");
+        targetProfileProperties.put("hive", "2.3.1");
+        targetProfileProperties.put("hadoop", "2.7.3");
         return new PluginClassifier(targetProfileProperties);
     }
 
